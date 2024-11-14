89d2046be465907981b4e3c009b20dacb55d269a
Mg Htoo <whshwe@gmail.com>
2024-11-14 10:37:04 +0630

    JS: separate js files for modularization

--- js/quiz.js
+++ js/quiz.js
@@ -1,32 +1,3 @@
-let totalNumOfQuestion = 5; // Set the total num of questions to show in the quiz.
-const mainContainer = document.querySelector("#main-container");
-const headerContainer = document.querySelector("#header");
-const questionContainer = document.querySelector("#question");
-const answersContainer = document.querySelector("#section-answers");
-const paginationContainer = document.querySelector("#section-pagination");
-const messageContainer = document.querySelector("#message");
-const nextButton = document.querySelector("#section-next-btn");
-const separatorContainer = document.querySelector("#section-separator");
-const reloadContainer = document.querySelector("#section-reload");
-let currentQuestionIndex; // Index to match the question and the answers.
-let currentQuestionNo = 1; // For paginations and to contol the number of questions to be shown.
-let questions = []; // Data will be fetch from JSON
-let shuffledQuestions = []; // To copy the questions array to manipulate without touching the original question array.
-
-const correctMessages = ["Fantastic!", "Awesome!", "Brilliant!", "Great job!", "Excellent!", "Superb!", "Outstanding!"];
-const wrongMessages = ["Almost there!", "Keep going!", "Nice effort!", "Keep practicing!", "Good try!"];
-
-const answerClassList = ["ans-a", "ans-b", "ans-c", "ans-d"];
-const alphabetImg = ["assets/a.png", "assets/b.png", "assets/c.png", "assets/d.png"];
-const alphabetAlt = ["a", "b", "c", "d"];
-
-let noOfTries = 1;
-let score = 0;
-
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 function randomQuestion() {
   // Simulate disabling the button after 0.3 seconds 
   // and add `disabled` class
