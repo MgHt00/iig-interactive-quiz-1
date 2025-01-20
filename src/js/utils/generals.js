fd67144eca37ce0eec6a1a7f4e3e1f37620649ba
Mg Htoo <whshwe@gmail.com>
2025-01-20 10:19:51 +0630

    generals.js - robust random()

--- src/js/utils/generals.js
+++ src/js/utils/generals.js
@@ -1,14 +1,15 @@
 export class Generals {
-  constructor() {
-    
-  }
 
   // To generate a random number
   random(min, max) {
     console.groupCollapsed("random()");
-    const randonNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (min > max) {
+      console.error("Invalid range: min should not be greater than max.");
+      return null;
+    }
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
     console.info("min:", min, "max:", max,"randonNumber:", randonNumber)
     console.groupEnd();
-    return randonNumber
+    return randomNumber;
   }
 }
