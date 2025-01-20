ee590f051b96304f4b81059a9829a117d5ae5a9b
Mg Htoo <whshwe@gmail.com>
2025-01-20 12:30:32 +0630

    generals.js: constructAbsoluteURL() to handle url with pathname

--- src/js/utils/generals.js
+++ src/js/utils/generals.js
@@ -14,14 +14,40 @@ export class Generals {
   }
 
   // Construct an absolute URL from a relative path
-  constructAbsoluteURL(path) {
+  /*constructAbsoluteURL(path) {
     console.groupCollapsed("constructAbsoluteURL()");
     if (!path.startsWith('/')) { // ensuring the path starts with a / to avoid any relative path issues.
       path = '/' + path;
     }
     const currentURL = new URL(window.location.href);
-    const absoluteURL = new URL(path, currentURL.origin);
-    console.info("Path:", path, "Absolute URL:", absoluteURL.href);
+    // captures the path up to the last /, excluding the file name (if present) but including the subdirectory.
+    const basePath = `${currentURL.origin}${currentURL.pathname.substring(0, currentURL.pathname.lastIndexOf('/'))}`; 
+    const absoluteURL = new URL(path, basePath);
+    console.info("Path:", path, "basePath:", basePath, "Absolute URL:", absoluteURL.href);
+    console.groupEnd();
+    return absoluteURL.href;
+  }*/
+
+  constructAbsoluteURL(path) {
+    console.groupCollapsed("constructAbsoluteURL()");
+    
+    // Check if path starts with '/'. Remove it to prevent absolute path behavior
+    // When the path is an absolute path (starting with /), it overrides the baseURL
+      if (path.startsWith('/')) {
+        path = path.substring(1);
+    }
+    console.info("path:", path);
+
+    const currentURL = new URL(window.location.href);
+    const urlOrigin = currentURL.origin;
+    const urlPathName = currentURL.pathname;
+
+    const baseURL = `${urlOrigin}${urlPathName}`;
+    console.info("baseURL:", baseURL);
+
+    const absoluteURL = new URL(path, baseURL);
+    console.info("absoluteURL.href:", absoluteURL.href);
+
     console.groupEnd();
     return absoluteURL.href;
   }
